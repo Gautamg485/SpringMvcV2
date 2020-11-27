fae17fdbb6f91cfdf32faef1896b074b3f1c191a
gowtham <gowtham@creditmantri.com>
2020-11-27 20:01:04 +0530

    First commit

---
+++ src/main/java/com/gowtham/SpringMvcV2/SpringMvcV2Application.java
@@ -0,0 +1,13 @@
+package com.gowtham.SpringMvcV2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcV2Application.class, args);
+	}
+
+}
